a7d4dc322651ee576cd8da49568fd7e9efccc02c
Batuhan Taş <batuhan.tas@yahoo.com>
2023-11-04 17:41:21 +0300

    Odev 3 Tamamlandi

---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "TechCareer Android Developer Odev 3"
+include(":app")
